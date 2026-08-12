5424cc1349cb6c5963746b105e937b3e832ac5eb
papermind-dev <dev@papermind.local>
2026-08-12 19:17:44 +0800

    chore: initialise React Native project with TypeScript

---
+++ babel.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    ['module-resolver', {
+      root: ['./src'],
+      alias: { '@': './src' },
+    }],
+    '@babel/plugin-proposal-decorators',
+  ],
+};
---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  preset: 'react-native',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?|@react-navigation|@nozbe|react-native-.*)/)',
+  ],
+};
